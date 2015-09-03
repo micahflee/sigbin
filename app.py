f55c77b4bcf03145fad69116b6fbf152ef6a9c07
Micah Lee <micah.lee@theintercept.com>
2015-09-03 16:05:29 -0700

    Refactor GnuPG class to encapsulate subprocess calls to gpg

--- app.py
+++ app.py
@@ -30,35 +30,41 @@ class GnuPG(object):
         # Default key server
         self.keyserver = 'hkp://pool.sks-keyservers.net'
 
-        # Base command to run
-        self.gpg_command = [self.gpg_path, '--batch', '--no-tty', '--keyserver', self.keyserver, '--homedir', self.homedir]
+        # Make sure we have a server key
+        self.seckey_fp = self.get_seckey_fp()
 
-    def is_gpg_available(self):
-        return os.path.isfile(self.gpg_path)
+    def get_seckey_fp(self):
+        return None
+
+    def _gpg(self, args, input=None):
+        p = Popen([self.gpg_path, '--batch', '--no-tty', '--keyserver', self.keyserver, '--homedir', self.homedir] + args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+        if input:
+            (stdoutdata, stderrdata) = p.communicate(input)
+        else:
+            p.wait()
+            stderrdata = p.stderr.read()
+
+        print stderrdata
+        return stderrdata
 
     def verify(self, text):
-        p = Popen(self.gpg_command + ['--verify'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-        (stdoutdata, stderrdata) = p.communicate(text)
-        print stderrdata
+        output = self._gpg(['--verify'], text)
 
         # Do we have the signing key?
-        if "Can't check signature: No public key" in stderrdata:
+        if "Can't check signature: No public key" in output:
             keyid = ''
-            for line in stderrdata.split('\n'):
+            for line in output.split('\n'):
                 if line.startswith('gpg: Signature made'):
                     keyid = line.split()[-1]
             if re.match(r'^[a-fA-F\d]{8}$', keyid):
                 # Try to fetch the signing key from key server
-                p = Popen(self.gpg_command + ['--recv-keys', keyid], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-                p.wait()
-                stderrdata = p.stderr.read()
-                print stderrdata
+                output = self._gpg(['--recv-keys', keyid])
 
-                if "key {} not found on keyserver".format(keyid) in stderrdata:
+                if "key {} not found on keyserver".format(keyid) in output:
                     return ("The signing key was not found on key servers", None)
                 else:
                     import_success = False
-                    for line in stderrdata.split('\n'):
+                    for line in output.split('\n'):
                         if line.startswith('gpg: key {}: public key "'.format(keyid)) and line.endswith('" imported'):
                             import_success = True
 
@@ -73,7 +79,7 @@ class GnuPG(object):
         # Was the signature good?
         good_sig = False
         fingerprint = None
-        for line in stderrdata.split('\n'):
+        for line in output.split('\n'):
             if line.startswith('gpg: Good signature from '):
                 good_sig = True
             if line.startswith('Primary key fingerprint: '):
